a5e5b371777e00af1cb9252cf255fc52ed6641d4
shravangopalakrishnan <116984791+shravangopalakrishnan@users.noreply.github.com>
2024-10-29 11:05:08 -0400

    Update packageDisplay.js
    Added Aura and LMS integration.

--- CountryPackageApp/packageDisplay/packageDisplay.js
+++ CountryPackageApp/packageDisplay/packageDisplay.js
@@ -1,10 +1,15 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import getPlansByCountry from '@salesforce/apex/PlanController.getPlansByCountry';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { publish, MessageContext } from 'lightning/messageService';
+import COUNTRY_PACKAGE_CHANNEL from '@salesforce/messageChannel/CountryPackageChannel__c';
+
 
 export default class PackageDisplay extends LightningElement {
     @api countryId;
     @track packages = [];
+    @wire(MessageContext) messageContext;
+
 
     @wire(getPlansByCountry, { countryId: '$countryId' })
     wiredPlans({ error, data }) {
@@ -20,6 +25,13 @@ export default class PackageDisplay extends LightningElement {
         this.showToast('Success', 'Proceeding to Booking', 'success');
         const proceedEvent = new CustomEvent('proceedtocustomerdetails', { detail: { planId } });
         this.dispatchEvent(proceedEvent);
+        console.log('Plan selected and published:', planId);
+
+        const message = { planId, countryId: this.countryId };
+        publish(this.messageContext, COUNTRY_PACKAGE_CHANNEL, message);
+
+        this.dispatchEvent(new CustomEvent('proceedtocustomerdetails', { detail: { planId } }));
+
     }
 
     showToast(title, message, variant) {
